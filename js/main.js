11a106b1e84b33a3eea96b97f9e83711ae2a50c3
Ryan Johnson <rpainterj@gmail.com>
2015-11-09 19:23:47 -0600

    optimized images, added favicon

--- js/main.js
+++ js/main.js
@@ -14,32 +14,11 @@ $(document).ready(function() {
       navigation: true,
       pagination : true
 
- 
   });
 
   $("#secondary-nav").stick_in_parent();
  
 });
-
-
-//Make Saved Artwork disappear
-
-// $('span.plus, span.minus').click(function(){
-// 	if (top.location.pathname === '/piece.html')
-// 	{
-// 	    $(this).toggleClass("plus minus");
-// 	}
-
-// 	else {
-
-// 	$(this).parent().parent().hide();
-
-// }
-	
-// });
-
-
-
 
 //Menu Toggle
 
@@ -47,16 +26,13 @@ $('.nav-toggle').click(function(){
   $('.mobile-header-nav').slideToggle();
 });
 
-
 $('#secondary-nav-mobile-toggle').click(function(){
   $('#secondary-nav-mobile').slideToggle();
 });
 
 
 
-
 //Active State for Menu
-
 var url = window.location.href;
 
 // Will only work if string in href matches with location
@@ -70,14 +46,31 @@ $('.header-nav a').filter(function() {
 
 
 // For toggling plus/minus classes
-
-
-
-
 $('span.plus, span.minus').click(function(){
     $(this).toggleClass("plus minus");
   
 });
+
+
+
+//Make Saved Artwork disappear
+
+// $('span.plus, span.minus').click(function(){
+//  if (top.location.pathname === '/piece.html')
+//  {
+//      $(this).toggleClass("plus minus");
+//  }
+
+//  else {
+
+//  $(this).parent().parent().hide();
+
+// }
+  
+// });
+
+
+
 
 
 
